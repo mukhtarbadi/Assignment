51b2a28df0c0757ecd2b0a88f37c2aa0f0cb2080
mukhtarbadi <127752593+mukhtarbadi@users.noreply.github.com>
2025-07-30 09:47:39 +0530

    Update and rename main (73).c to 08.c

--- Module-2/Lab Exercise/08.c
+++ Module-2/Lab Exercise/08.c
@@ -2,47 +2,36 @@
 them. Extend this to handle a two-dimensional array (3x3 matrix) and 
 calculate the sum of all elements.*/
 
-
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
 
 int main() {
-    
-    int arr[5],i,j;
+    int arr[5], i, j;
+
     printf("Enter 5 integers for 1D array:\n");
-    
-    for(i = 0; i < 5; i++) 
-	{
-        printf("Element %d: ", i+1);
+    for(i = 0; i < 5; i++) {
+        printf("Element %d: ", i + 1);
         scanf("%d", &arr[i]);
     }
 
     printf("\nThe 1D array elements are:\n");
-    for(i = 0; i < 5; i++)
-	{
+    for(i = 0; i < 5; i++) {
         printf("%d ", arr[i]);
     }
 
-    
     int matrix[3][3], sum = 0;
-    
+
     printf("\n\nEnter 9 integers for 3x3 matrix:\n");
-    
-    for(i = 0; i < 3; i++) 
-	{
-        for(j = 0; j < 3; j++) 
-		{
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
             printf("Element [%d][%d]: ", i, j);
             scanf("%d", &matrix[i][j]);
-            sum += matrix[i][j]; 
+            sum += matrix[i][j];
         }
     }
 
     printf("\nThe 3x3 matrix is:\n");
-    for(i = 0; i < 3; i++) 
-	{
-        for(j = 0; j < 3; j++) 
-		{
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
             printf("%d\t", matrix[i][j]);
         }
         printf("\n");
