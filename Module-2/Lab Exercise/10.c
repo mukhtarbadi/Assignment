2047b8a96531c08732602cb8547c1de3383cd286
mukhtarbadi <127752593+mukhtarbadi@users.noreply.github.com>
2025-07-30 09:49:17 +0530

    Update and rename main (75).c to 10.c

--- Module-2/Lab Exercise/10.c
+++ Module-2/Lab Exercise/10.c
@@ -2,26 +2,22 @@
 using strcat(). Display the concatenated string and its length using 
 strlen(). */
 
-
 #include <stdio.h>
-#include <conio.h>
 #include <string.h>
 
 int main() {
     char str1[100], str2[100];
 
-    
     printf("Enter the first string: ");
-    scanf("%s", str1);  
+    scanf("%s", str1);
 
     printf("Enter the second string: ");
     scanf("%s", str2);
 
     strcat(str1, str2);
 
-
-    printf("\n Concatenated String: %s\n", str1);
-    printf("\n Length of Concatenated String: %d", strlen(str1));
+    printf("\nConcatenated String: %s\n", str1);
+    printf("Length of Concatenated String: %lu\n", strlen(str1));
 
     return 0;
 }
