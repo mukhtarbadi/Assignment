c053661df0a40d75f47e661ce564df0b9bce9abc
mukhtarbadi <127752593+mukhtarbadi@users.noreply.github.com>
2025-07-30 10:43:37 +0530

    Update Fibonacci Sequence.c

--- Module-2/Extra Lab Exercises/Fibonacci Sequence.c
+++ Module-2/Extra Lab Exercises/Fibonacci Sequence.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include <time.h>
 
-// Recursive function
 int fibonacci_recursive(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
     return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
 }
 
-// Iterative function
 int fibonacci_iterative(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
 
-    int a = 0, b = 1, c, i;
-    for(i = 2; i <= n; i++) {
+    int a = 0, b = 1, c;
+    for (int i = 2; i <= n; i++) {
         c = a + b;
         a = b;
         b = c;
@@ -30,13 +28,11 @@ int main() {
     printf("Enter the term number (N): ");
     scanf("%d", &N);
 
-    // Measure recursive time
     start = clock();
     int fib_rec = fibonacci_recursive(N);
     end = clock();
     time_recursive = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-    // Measure iterative time
     start = clock();
     int fib_itr = fibonacci_iterative(N);
     end = clock();
@@ -48,9 +44,9 @@ int main() {
     printf("Nth Fibonacci number (iterative): %d\n", fib_itr);
     printf("Time taken (iterative): %f seconds\n", time_iterative);
 
-    if(time_recursive > time_iterative)
+    if (time_recursive > time_iterative)
         printf("Iterative method is faster.\n");
-    else if(time_recursive < time_iterative)
+    else if (time_recursive < time_iterative)
         printf("Recursive method is faster.\n");
     else
         printf("Both methods have similar performance.\n");
