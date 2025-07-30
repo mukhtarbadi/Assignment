f2effc9b322706f61d8d07a02baa65608b0bd7ff
mukhtarbadi <127752593+mukhtarbadi@users.noreply.github.com>
2025-07-30 10:39:06 +0530

    Update Check Number Properties.c

--- Module-2/Extra Lab Exercises/Check Number Properties.c
+++ Module-2/Extra Lab Exercises/Check Number Properties.c
@@ -1,19 +1,18 @@
+//Write a C program to check if a number is even or odd, determine its sign (positive, negative, or zero), and check if it is a multiple of both 3 and 5.
+
 #include <stdio.h>
 
 int main() {
     int number;
 
-    // Input from user
     printf("Enter an integer: ");
     scanf("%d", &number);
 
-    // Check even or odd using modulus operator
     if (number % 2 == 0)
         printf("The number is even.\n");
     else
         printf("The number is odd.\n");
 
-    // Check sign using relational operators
     if (number > 0)
         printf("The number is positive.\n");
     else if (number < 0)
@@ -21,7 +20,6 @@ int main() {
     else
         printf("The number is zero.\n");
 
-    // Check if number is a multiple of both 3 and 5
     if (number % 3 == 0 && number % 5 == 0)
         printf("The number is a multiple of both 3 and 5.\n");
     else
