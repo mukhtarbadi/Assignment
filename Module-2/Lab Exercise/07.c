22a8872af58ce0b72d437b48d77d85fce300d834
mukhtarbadi <127752593+mukhtarbadi@users.noreply.github.com>
2025-07-30 09:46:28 +0530

    Update and rename main (72).c to 07.c

--- Module-2/Lab Exercise/07.c
+++ Module-2/Lab Exercise/07.c
@@ -2,38 +2,29 @@
 Include function declaration, definition, and call. */
 
 #include <stdio.h>
-#include <conio.h>
 
-// Function declaration
 int factorial(int n);
 
-int main()   
-{
+int main() {
     int number, result;
 
     printf("Enter a positive integer: ");
     scanf("%d", &number);
 
-    if(number < 0) 
-	{
+    if (number < 0) {
         printf("Factorial is not defined for negative numbers.\n");
-    } 
-	else 
-	{
-        // Function call
+    } else {
         result = factorial(number);
         printf("Factorial of %d is %d\n", number, result);
     }
 
-    return 0;  
+    return 0;
 }
 
-// Function definition
-int factorial(int n)
-{
-    int fact = 1;\
+int factorial(int n) {
+    int fact = 1;
     int i;
-    for(i = 1; i <= n; i++) {
+    for (i = 1; i <= n; i++) {
         fact = fact * i;
     }
     return fact;
