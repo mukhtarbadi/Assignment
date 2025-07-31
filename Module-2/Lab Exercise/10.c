38dc12eaf2e2d3f1e173c67ddaa1e495b08372d7
mukhtarbadi <127752593+mukhtarbadi@users.noreply.github.com>
2025-07-31 12:57:02 +0530

    Update 10.c

--- Module-2/Lab Exercise/10.c
+++ Module-2/Lab Exercise/10.c
@@ -1,23 +1,20 @@
-/*Write a C program that takes two strings from the user and concatenates them 
-using strcat(). Display the concatenated string and its length using 
-strlen(). */
-
 #include <stdio.h>
 #include <string.h>
+
+// This is a program to join two strings and find the length
 
 int main() {
     char str1[100], str2[100];
 
-    printf("Enter the first string: ");
-    scanf("%s", str1);
+    printf("Enter first string: ");
+    gets(str1);  
+    printf("Enter second string: ");
+    gets(str2);
 
-    printf("Enter the second string: ");
-    scanf("%s", str2);
+    strcat(str1, str2); 
 
-    strcat(str1, str2);
-
-    printf("\nConcatenated String: %s\n", str1);
-    printf("Length of Concatenated String: %lu\n", strlen(str1));
+    printf("\nConcatenated string: %s", str1);
+    printf("\nLength of string: %lu", strlen(str1));
 
     return 0;
 }
