dc0d656edcc67f986a13c0a97e548f4943dc9218
mukhtarbadi <127752593+mukhtarbadi@users.noreply.github.com>
2025-07-31 12:37:35 +0530

    Update 05.c

--- Module-2/Lab Exercise/05.c
+++ Module-2/Lab Exercise/05.c
@@ -1,25 +1,27 @@
-/*Write a C program to print numbers from 1 to 10 using all three types of loops 
-(while, for, do-while).*/
-
 #include <stdio.h>
+
+// This is a program to print numbers from 1 to 10 using while, for, and do-while loops
 
 int main() {
     int i;
 
-    printf("Using for loop: ");
-    for (i = 1; i <= 10; i++) {
-        printf("%d ", i);
-    }
-
-    printf("\nUsing while loop: ");
+    // Using while loop
     i = 1;
+    printf("While Loop:\n");
     while (i <= 10) {
         printf("%d ", i);
         i++;
     }
 
-    printf("\nUsing do-while loop: ");
+    // Using for loop
+    printf("\n\nFor Loop:\n");
+    for (i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+
+    // Using do-while loop
     i = 1;
+    printf("\n\nDo-While Loop:\n");
     do {
         printf("%d ", i);
         i++;
