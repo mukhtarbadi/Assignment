88e841d885d26a4fc855022d56b0f00172543a8b
mukhtarbadi <127752593+mukhtarbadi@users.noreply.github.com>
2025-07-31 12:40:31 +0530

    Update 06.c

--- Module-2/Lab Exercise/06.c
+++ Module-2/Lab Exercise/06.c
@@ -1,26 +1,26 @@
-/*Write a C program that uses the break statement to stop printing numbers 
-when it reaches 5. Modify the program to skip printing the number 3 using the 
-continue statement. */
-
 #include <stdio.h>
+
+// This is a program to use break and continue statements with a loop
 
 int main() {
     int i;
 
-    printf("Stop printing numbers when it reaches 5:\n");
+    // Using break to stop at 5
+    printf("Using break (stop at 5):\n");
     for (i = 1; i <= 10; i++) {
         if (i == 5) {
             break;
         }
-        printf("%d\n", i);
+        printf("%d ", i);
     }
 
-    printf("\nSkip printing the number 3 using the continue statement:\n");
-    for (i = 1; i <= 10; i++) {
+    // Using continue to skip 3
+    printf("\n\nUsing continue (skip 3):\n");
+    for (i = 1; i <= 5; i++) {
         if (i == 3) {
             continue;
         }
-        printf("%d\n", i);
+        printf("%d ", i);
     }
 
     return 0;
